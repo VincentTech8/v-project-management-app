1c079bc5c408eb10128db8d1003c32ab915022a8
vinctechgeek <toxicgeek8@gmail.com>
2024-12-08 20:06:42 +1100

    Add s3 URL

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -64,7 +64,7 @@ const SideBar = () => {
         </div>
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <Image src="pm-s3-images-vc.s3.us-east-1.amazonaws.com/logo.png" alt="Logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               VINN TEAM
